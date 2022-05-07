d09fd5c8701a4b036280b18ac230a897081ea799
Briceuuuh <brice.huet@epitech.eu>
2022-05-07 16:12:19 +0400

    log screen mvp

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
-import React, { FC } from "react";
-import {View, Text, StyleSheet} from "react-native";
+import React from "react";
+import {View, Text, StyleSheet, Button, TextInput, ScrollView, ImageBackground} from "react-native";
 
-const App: FC = () => {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Your name"
+      />
+      <TextInput
+      secureTextEntry={true}
+        style={styles.input}
+        placeholder="Password"
+      />
+      <Button
+        title="Login"
+      />
     </View>
   );
 };
@@ -13,8 +24,14 @@ export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    height: 70,
+    margin: 50,
+    padding: 10,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
